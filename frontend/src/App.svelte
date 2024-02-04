881476468969d94f5139d1ff2636418a8e85bdff
Gitesh Sharma <58145476+glitchedgitz@users.noreply.github.com>
2024-02-04 19:30:56 +0530

    added `frontend` to compile with binary

---
+++ frontend/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { App } from "cybernetic-ui";
+  import "/node_modules/cybernetic-ui/dist/app.css";
+  import "./assets/fonts/fonts.css";
+</script>
+
+<div class="block fixed top-0 left-0 w-full h-full">
+  <App />
+</div>
