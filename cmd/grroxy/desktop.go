6dfbf6167b396d298545549c3e4f9005ba885675
Gitesh Sharma <58145476+glitchedgitz@users.noreply.github.com>
2025-06-17 15:39:43 +0530

    wails config udpate for mac

--- cmd/grroxy/desktop.go
+++ cmd/grroxy/desktop.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"context"
+	"runtime"
 
 	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/menu"
+	"github.com/wailsapp/wails/v2/pkg/menu/keys"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
+	rt "github.com/wailsapp/wails/v2/pkg/runtime"
 
 	"github.com/glitchedgitz/grroxy-db/cmd/grroxy/frontend"
 )
@@ -26,16 +31,50 @@ func (a *App) GetHost() string {
 	return "http://" + MainHostAddress
 }
 
+func FindChromePath() string {
+	chromePath := ""
+	if runtime.GOOS == "darwin" {
+		chromePath = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+	}
+	return chromePath
+}
+
 func runApp() {
+	frameless := true
 	app := NewApp()
+
+	AppMenu := menu.NewMenu()
+	if runtime.GOOS == "darwin" {
+		AppMenu.Append(menu.AppMenu()) // On macOS platform, this must be done right after `NewMenu()`
+	}
+	FileMenu := AppMenu.AddSubmenu("File")
+	FileMenu.AddText("&Open", keys.CmdOrCtrl("o"), func(_ *menu.CallbackData) {
+		// do something
+	})
+	FileMenu.AddSeparator()
+	FileMenu.AddText("Quit", keys.CmdOrCtrl("q"), func(_ *menu.CallbackData) {
+		rt.Quit(app.ctx)
+	})
+
+	FileMenu.AddSeparator()
+	FileMenu.AddText("Fullscreen", keys.CmdOrCtrl("f"), func(_ *menu.CallbackData) {
+		rt.WindowFullscreen(app.ctx)
+	})
+
+	if runtime.GOOS == "darwin" {
+		frameless = false
+		AppMenu.Append(menu.EditMenu()) // On macOS platform, EditMenu should be appended to enable Cmd+C, Cmd+V, Cmd+Z... shortcuts
+	}
+
 	// conf.Initiate()
 	// conf.LoadAppData()
 	err := wails.Run(&options.App{
 		Title:            "Grroxy",
 		Width:            1366,
 		Height:           768,
-		Frameless:        true,
+		Frameless:        frameless,
 		WindowStartState: options.Maximised,
+		Menu:             AppMenu, // reference the menu above
 		AssetServer: &assetserver.Options{
 			Assets: frontend.DistDirFS,
 		},
@@ -43,6 +82,9 @@ func runApp() {
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
+		},
+		Mac: &mac.Options{
+			TitleBar: mac.TitleBarHiddenInset(),
 		},
 	})
 
